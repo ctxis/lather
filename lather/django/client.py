087c59d405e1364ade043a5cf689496e33cf41d3
Lefteris Nikoltsios <Lefteris.Nikoltsios@contextis.co.uk>
2016-06-22 16:01:26 +0100

    Parse multiple values in django client initialization.

--- lather/django/client.py
+++ lather/django/client.py
@@ -2,8 +2,5 @@
 from django.conf import settings
 from lather.client import LatherClient
 
-base_url = getattr(settings, 'LATHER_BASE_URL', None)
-username = getattr(settings, 'LATHER_USERNAME', None)
-password = getattr(settings, 'LATHER_PASSWORD', None)
-
-lather_client = LatherClient(base_url, username, password)
+options = getattr(settings, 'LATHER_CONFIG', None)
+lather_client = LatherClient(**options)
