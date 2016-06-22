9eb7ff14c6f7657063a2322adc128f3b928719d4
Lefteris Nikoltsios <Lefteris.Nikoltsios@contextis.co.uk>
2016-06-22 13:49:49 +0100

    Add intergration with django.

---
+++ lather/django/client.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from django.conf import settings
+from lather.client import LatherClient
+
+
+base_url = getattr(settings, 'LATHER_BASE_URL', None)
+username = getattr(settings, 'LATHER_USERNAME', None)
+password = getattr(settings, 'LATHER_PASSWORD', None)
+
+lather_client = LatherClient(base_url, username, password)
